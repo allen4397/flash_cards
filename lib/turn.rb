aa573c5f3cef7bd936fc89b96549e1f3153bce31
Tim Allen <tnelsallen3@gmail.com>
2018-11-27 17:45:59 -0700

    Adjusts names

---
+++ lib/turn.rb
@@ -0,0 +1,9 @@
+class Turn
+  def initialize(string, Card)
+    @guess = string
+  end
+
+  def guess
+    @guess
+  end
+end
---
+++ test/turn_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turns'
+
+class TurnTest < Minitest::Test
+
+end
