416490238e2a9eed7ffcf0476d9ff890cf32c342
Tim Allen <tnelsallen3@gmail.com>
2018-11-27 17:48:51 -0700

    Deleted extra files

--- lib/turns.rb
+++
@@ -1,9 +0,0 @@
-class Turn
-  def initialize(string, Card)
-    @guess = string
-  end
-
-  def guess
-    @guess
-  end
-end
--- test/turns_test.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/turns'
-
-class TurnTest < Minitest::Test
-
-end
