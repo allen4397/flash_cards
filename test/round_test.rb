a1b4e9d7824c28cbed0bc5cc639a8b4bb64babe6
Tim Allen <tnelsallen3@gmail.com>
2018-11-29 14:59:04 -0700

    Adds updates current card test

--- test/round_test.rb
+++ test/round_test.rb
@@ -73,4 +73,16 @@ class RoundTest < Minitest::Test
 
     assert_equal [new_turn], round.turns
   end
+
+  def test_it_updates_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+
+    assert_equal card_2, round.current_card
+  end
 end
