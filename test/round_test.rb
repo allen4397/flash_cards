3f521a10e9cf1656668794aaaad3b708e2398a2f
Tim Allen <tnelsallen3@gmail.com>
2018-11-29 13:39:10 -0700

    Adds deck test

--- test/round_test.rb
+++ test/round_test.rb
@@ -2,11 +2,24 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/round.rb'
+require './lib/deck.rb'
+require './lib/card.rb'
 
 class RoundTest < Minitest::Test
   def test_it_exists
-    round = Round.new
+    round = Round.new("")
 
     assert_instance_of Round, round
+  end
+
+  def test_it_has_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal deck, round.deck
   end
 end
