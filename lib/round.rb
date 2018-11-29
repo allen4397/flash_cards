dec787f64e3e4c984379578672c5ade9a8c32ad5
Tim Allen <tnelsallen3@gmail.com>
2018-11-29 14:18:53 -0700

    Adds current_card attr

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,11 @@
 class Round
   attr_reader :deck,
-              :turns
+              :turns,
+              :current_card
 
   def initialize(deck, turns=[])
     @deck = deck
     @turns = turns
+    @current_card = deck.cards.first
   end
 end
