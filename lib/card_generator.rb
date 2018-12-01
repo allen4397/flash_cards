eba5bb89bdd4c7a022d8bf3a1c7b91f171aa8b87
Tim Allen <tnelsallen3@gmail.com>
2018-12-01 13:00:19 -0700

    Adds require csv to both files

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,2 +1,4 @@
+require 'csv'
+
 class CardGenerator
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -2,11 +2,18 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card_generator'
+require 'csv'
 
 class CardGeneratorTest < Minitest::Test
   def test_it_exists
     card_generator = CardGenerator.new
 
     assert_instance_of CardGenerator, card_generator
+  end
+
+  def test_it_contains_a_file
+    card_generator = CardGenerator.new("cards.txt")
+
+    assert_equals cards.txt, card_generator.file
   end
 end
