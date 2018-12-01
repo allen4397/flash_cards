65a826cf4297d7d6c3bdaa3e2fa8ee1232b4ac7b
Tim Allen <tnelsallen3@gmail.com>
2018-12-01 12:26:44 -0700

    Adds test it exists

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -2,3 +2,11 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card_generator'
+
+class CardGeneratorTest < Minitest::Test
+  def test_it_exists
+    card_generator = CardGenerator.new
+
+    assert_instance_of CardGenerator, card_generator
+  end
+end
