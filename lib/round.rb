d0b3ad78d515d569f8ffea98aef155e9458301ed
Tim Allen <tnelsallen3@gmail.com>
2018-12-01 11:36:16 -0700

    Refactored percent correct by category and finished start method

--- lib/round.rb
+++ lib/round.rb
@@ -17,10 +17,10 @@ class Round
   end
 
   def take_turn(guess)
-    if @categories.include?(@current_card.category) == false
-      @categories << @current_card.category
+    if categories.include?(current_card.category) == false
+      @categories << current_card.category
     end
-    new_turn = Turn.new(guess, @current_card)
+    new_turn = Turn.new(guess, current_card)
     @turns << new_turn
     @current_card = deck.cards.rotate!.first
     if new_turn.correct?
@@ -31,7 +31,7 @@ class Round
 
   def number_correct_by_category(category)
     total = 0
-    @turns.each do |turn|
+    turns.each do |turn|
       if turn.correct? && category == turn.card.category
         total += 1
       end
@@ -42,7 +42,7 @@ class Round
   def percent_correct
     correct = 0
     total = 0
-    @turns.each do |turn|
+    turns.each do |turn|
       if turn.correct?
         correct += 1
         total += 1
@@ -54,21 +54,29 @@ class Round
   end
 
   def percent_correct_by_category(category)
-    correct = 0
-    total = 0
-    @turns.each do |turn|
-      if turn.correct? && category == turn.card.category
-        correct += 1
-        total += 1
-      elsif category == turn.card.category
-        total += 1
+    turns_in_category = 0
+    turns.each do |turn|
+      if turn.card.category == category
+        turns_in_category += 1
       end
     end
-    return correct.to_f * 100 / total
+    number_correct_by_category(category).to_f * 100 / turns_in_category
   end
 
   def start
     puts "Welcome! You're playing with #{deck.cards.count} cards."
     puts "-------------------------------------------------"
+    while turns.count != deck.cards.count
+      puts "This is card number #{turns.count + 1} out of #{deck.cards.count}."
+      puts "Question: #{current_card.question}"
+      answer = gets.chomp
+      turn = take_turn(answer)
+      puts turn.feedback
+    end
+    puts "****** Game over! ******"
+    puts "You had #{number_correct} correct guesses out of #{turns.count} for a total score of #{percent_correct.to_i}%"
+    categories.each do |category|
+      puts "#{category} - #{percent_correct_by_category(category).to_i}% correct"
+    end
   end
 end
