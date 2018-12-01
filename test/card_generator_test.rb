aaf6e7917ae7018f79388ab37c7e70149211f089
Tim Allen <tnelsallen3@gmail.com>
2018-12-01 13:34:04 -0700

    Adjusts first test argument

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -6,7 +6,7 @@ require './lib/card_generator'
 
 class CardGeneratorTest < Minitest::Test
   def test_it_exists
-    card_generator = CardGenerator.new("")
+    card_generator = CardGenerator.new("cards.txt")
 
     assert_instance_of CardGenerator, card_generator
   end
